173be270ba4b7b986ebfb35c05e7d83267d08f04
baranovrp <baranov.r.p@gmail.com>
2018-09-22 10:34:51 +0200

    HW0 Filter meals by time with exceeded mark

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,6 +1,10 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 public class UserMeal {
     private final LocalDateTime dateTime;
@@ -9,7 +13,8 @@ public class UserMeal {
 
     private final int calories;
 
-    public UserMeal(LocalDateTime dateTime, String description, int calories) {
+    public UserMeal(final LocalDateTime dateTime,
+                    final String description, final int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -25,5 +30,39 @@ public class UserMeal {
 
     public int getCalories() {
         return calories;
+    }
+
+    public LocalDate getDate() {
+        return getDateTime().toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return getDateTime().toLocalTime();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserMeal that = (UserMeal) o;
+
+        return Objects.equals(this.dateTime, that.dateTime)
+            && Objects.equals(this.description, that.description)
+            && Objects.equals(this.calories, that.calories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dateTime, description, calories);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", this.getClass().getSimpleName() + "[", "]")
+            .add("dateTime = " + dateTime)
+            .add("description = " + description)
+            .add("calories = " + calories)
+            .toString();
     }
 }
--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 public class UserMealWithExceed {
     private final LocalDateTime dateTime;
@@ -11,10 +13,39 @@ public class UserMealWithExceed {
 
     private final boolean exceed;
 
-    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public UserMealWithExceed(final LocalDateTime dateTime, final String description,
+                              final int calories, final boolean exceed) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.exceed = exceed;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserMealWithExceed that = (UserMealWithExceed) o;
+
+        return Objects.equals(this.dateTime, that.dateTime)
+            && Objects.equals(this.description, that.description)
+            && Objects.equals(this.calories, that.calories)
+            && Objects.equals(this.exceed, that.exceed);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dateTime, description, calories, exceed);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", this.getClass().getSimpleName() + "[", "]")
+            .add("dateTime = " + dateTime)
+            .add("description = " + description)
+            .add("calories = " + calories)
+            .add("exceed = " + exceed)
+            .toString();
     }
 }
