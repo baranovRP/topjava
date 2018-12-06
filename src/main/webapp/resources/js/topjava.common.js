d280d5b059a1dfeda7601e179bea8f0ac40d97f0
baranovrp <baranov.r.p@gmail.com>
2018-12-06 17:41:38 +0100

    9 01 HW8

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -12,19 +12,8 @@ function makeEditable(ctx) {
 }
 
 function add() {
-    $("#modalTitle").html(i18n["addTitle"]);
     form.find(":input").val("");
     $("#editRow").modal();
-}
-
-function updateRow(id) {
-    $("#modalTitle").html(i18n["editTitle"]);
-    $.get(context.ajaxUrl + id, function (data) {
-        $.each(data, function (key, value) {
-            form.find("input[name='" + key + "']").val(value);
-        });
-        $('#editRow').modal();
-    });
 }
 
 function deleteRow(id) {
@@ -33,7 +22,7 @@ function deleteRow(id) {
         type: "DELETE"
     }).done(function () {
         context.updateTable();
-        successNoty("common.deleted");
+        successNoty("Deleted");
     });
 }
 
@@ -49,7 +38,7 @@ function save() {
     }).done(function () {
         $("#editRow").modal("hide");
         context.updateTable();
-        successNoty("common.saved");
+        successNoty("Saved");
     });
 }
 
@@ -62,10 +51,10 @@ function closeNoty() {
     }
 }
 
-function successNoty(key) {
+function successNoty(text) {
     closeNoty();
     new Noty({
-        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + i18n[key],
+        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
         type: 'success',
         layout: "bottomRight",
         timeout: 1000
@@ -75,20 +64,8 @@ function successNoty(key) {
 function failNoty(jqXHR) {
     closeNoty();
     failedNote = new Noty({
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
         type: "error",
         layout: "bottomRight"
     }).show();
-}
-
-function renderEditBtn(data, type, row) {
-    if (type === "display") {
-        return "<a onclick='updateRow(" + row.id + ");'><span class='fa fa-pencil'></span></a>";
-    }
-}
-
-function renderDeleteBtn(data, type, row) {
-    if (type === "display") {
-        return "<a onclick='deleteRow(" + row.id + ");'><span class='fa fa-remove'></span></a>";
-    }
 }
--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -1,29 +1,8 @@
-const userAjaxUrl = "ajax/admin/users/";
-
-function enable(chkbox, id) {
-    const enabled = chkbox.is(":checked");
-//  https://stackoverflow.com/a/22213543/548473
-    $.ajax({
-        url: userAjaxUrl + id,
-        type: "POST",
-        data: "enabled=" + enabled
-    }).done(function () {
-        chkbox.closest("tr").attr("data-userEnabled", enabled);
-        successNoty(enabled ? "common.enabled" : "common.disabled");
-    }).fail(function () {
-        $(chkbox).prop("checked", !enabled);
-    });
-}
-
 // $(document).ready(function () {
 $(function () {
     makeEditable({
-            ajaxUrl: userAjaxUrl,
+            ajaxUrl: "ajax/admin/users/",
             datatableApi: $("#datatable").DataTable({
-                "ajax": {
-                    "url": userAjaxUrl,
-                    "dataSrc": ""
-                },
                 "paging": false,
                 "info": true,
                 "columns": [
@@ -31,44 +10,24 @@ $(function () {
                         "data": "name"
                     },
                     {
-                        "data": "email",
-                        "render": function (data, type, row) {
-                            if (type === "display") {
-                                return "<a href='mailto:" + data + "'>" + data + "</a>";
-                            }
-                            return data;
-                        }
+                        "data": "email"
                     },
                     {
                         "data": "roles"
                     },
                     {
-                        "data": "enabled",
-                        "render": function (data, type, row) {
-                            if (type === "display") {
-                                return "<input type='checkbox' " + (data ? "checked" : "") + " onclick='enable($(this)," + row.id + ");'/>";
-                            }
-                            return data;
-                        }
+                        "data": "enabled"
                     },
                     {
-                        "data": "registered",
-                        "render": function (date, type, row) {
-                            if (type === "display") {
-                                return date.substring(0, 10);
-                            }
-                            return date;
-                        }
+                        "data": "registered"
                     },
                     {
-                        "orderable": false,
-                        "defaultContent": "",
-                        "render": renderEditBtn
+                        "defaultContent": "Edit",
+                        "orderable": false
                     },
                     {
-                        "orderable": false,
-                        "defaultContent": "",
-                        "render": renderDeleteBtn
+                        "defaultContent": "Delete",
+                        "orderable": false
                     }
                 ],
                 "order": [
@@ -76,15 +35,10 @@ $(function () {
                         0,
                         "asc"
                     ]
-                ],
-                "createdRow": function (row, data, dataIndex) {
-                    if (!data.enabled) {
-                        $(row).attr("data-userEnabled", false);
-                    }
-                }
+                ]
             }),
             updateTable: function () {
-                $.get(userAjaxUrl, updateTableByData);
+                $.get("ajax/admin/users/", updateTableByData);
             }
         }
     );
