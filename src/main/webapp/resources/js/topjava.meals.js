905d54bea1ce90bd646d56e2799c96c3849b827d
baranovrp <baranov.r.p@gmail.com>
2018-12-11 18:59:15 +0100

    HW9 Use datetimepicker

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -58,3 +58,17 @@ $(function () {
         updateTable: updateFilteredTable
     });
 });
+
+$("#startDate, #endDate").datetimepicker({
+    timepicker: false,
+    format: "Y-m-d"
+});
+
+$("#startTime, #endTime").datetimepicker({
+    datepicker: false,
+    format: "H:i"
+});
+
+$("#dateTime").datetimepicker({
+    format:"Y-m-d\\TH:i:s"
+});
